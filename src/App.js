00cfae5d80528597705d18d15fe892dc6bd6599e
khavaa <khavaom@gmail.com>
2024-12-10 12:27:39 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Alumni</h1>
+        <p>EKOD Alumni</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
